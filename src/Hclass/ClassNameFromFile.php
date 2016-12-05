8f0e5a8565e82616753b6714aa42063239aafbd6
夏琳泰 <johndoe@example.com>
2016-12-06 00:17:44 +0800

    添加：根据文件分析类

---
+++ src/Hclass/ClassNameFromFile.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-11-30
+ * Time: 下午 12:57.
+ */
+
+namespace xltxlm\Hclass;
+
+/**
+ * 从一个文件中解析出类名,前提：符合psr-4命名方式
+ * Class CLassNameFromFile.
+ */
+final class ClassNameFromFile
+{
+    /** @var string 文件路径 */
+    protected $filePath = '';
+    /** @var string 类名称 */
+    private $className = '';
+    /** @var string 类的命名空间 */
+    private $nameSpace = '';
+
+    /**
+     * @return string
+     */
+    public function getNameSpace(): string
+    {
+        return $this->nameSpace;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFilePath(): string
+    {
+        return $this->filePath;
+    }
+
+    /**
+     * @param string $filePath
+     *
+     * @return ClassNameFromFile
+     */
+    public function setFilePath(string $filePath): ClassNameFromFile
+    {
+        $token = token_get_all(file_get_contents($filePath));
+        $namespace = false;
+        foreach ($token as $item) {
+            if (is_array($item)) {
+                $item[0] = token_name($item[0]);
+                if ($namespace !== false && $item[0] == 'T_STRING') {
+                    $this->nameSpace .= $item[1].'\\';
+                }
+                if ($item[0] == 'T_NAMESPACE') {
+                    $namespace = true;
+                }
+            }
+            //遇见分号的时候，命名空间才结束
+            if ($namespace!==false && $item == ';') {
+                break;
+            }
+        }
+        $this->className = $this->getNameSpace().basename($filePath, '.php');
+        $this->filePath = $filePath;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClassName(): string
+    {
+        return $this->className;
+    }
+}
---
+++ test/ClassNameFromFileTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use xltxlm\Hclass\ClassNameFromFile;
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-11-30
+ * Time: 下午 4:10.
+ */
+class ClassNameFromFileTest extends TestCase
+{
+    /**
+     * 一维名命名空间的.
+     */
+    public function test()
+    {
+        $className = (new ClassNameFromFile())
+            ->setFilePath(__FILE__)
+            ->getClassName();
+        $this->assertEquals(ClassNameFromFileTest::class, $className);
+    }
+}
