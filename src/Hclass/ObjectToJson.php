f44898e268e63bcc42f0156cb250f7697b487a3b
夏琳泰 <johndoe@example.com>
2016-12-06 23:12:04 +0800

    新增 对象 => json

---
+++ src/Hclass/ObjectToJson.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-12-06
+ * Time: 下午 11:05
+ */
+
+namespace xltxlm\helper\Hclass;
+
+/**
+ * 把对象输出成json格式
+ * Class ObjectToJson
+ * @package xltxlm\helper\Hclass
+ */
+trait ObjectToJson
+{
+    final public function __toString()
+    {
+        return json_encode(get_object_vars($this), JSON_UNESCAPED_UNICODE);
+    }
+}
---
+++ tests/ObjectToJsonTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-12-06
+ * Time: 下午 11:10
+ */
+
+namespace xltxlm\helper\tests;
+
+
+use PHPUnit\Framework\TestCase;
+use xltxlm\helper\tests\Resource\ObjectToJson\Demo;
+
+class ObjectToJsonTest extends TestCase
+{
+
+    public function test1()
+    {
+        $a = (new Demo());
+        $this->assertEquals($a, '{"id":17,"name":18,"my_code":19}');
+    }
+}
