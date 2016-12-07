1f9c2b98ef14e2e6b65e16d1449b764fe2f7e0d2
夏琳泰 <johndoe@example.com>
2016-12-07 10:58:40 +0800

    新增 xml => 数组

---
+++ src/Harray/XmlToArray.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-12-07
+ * Time: 上午 10:52
+ */
+
+namespace xltxlm\helper\Harray;
+
+/**
+ * 一维的xml解析成数组
+ * Class Xml2Array
+ * @package xltxlm\helper\Harray
+ */
+class XmlToArray
+{
+    protected $xml = "";
+
+    /**
+     * @return string
+     */
+    public function getXml(): string
+    {
+        return $this->xml;
+    }
+
+    /**
+     * @param string $xml
+     * @return XmlToArray
+     */
+    public function setXml(string $xml): XmlToArray
+    {
+        $this->xml = $xml;
+        return $this;
+    }
+
+    public function __invoke()
+    {
+        $parser = xml_parser_create();
+        xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+        xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+        xml_parse_into_struct($parser, $this->xml, $values, $tags);
+        xml_parser_free($parser);
+        array_pop($values);
+        array_shift($values);
+        // 遍历 XML 结构
+        $tdb = [];
+        foreach ($values as $key => $val) {
+            $tdb[$val['tag']] = $val['value'];
+        }
+        return $tdb;
+    }
+}
---
+++ tests/Harray/XmlToArrayTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-12-07
+ * Time: 上午 10:54
+ */
+
+namespace xltxlm\helper\tests\Harray;
+
+
+use PHPUnit\Framework\TestCase;
+use xltxlm\helper\Harray\XmlToArray;
+
+class XmlToArrayTest extends TestCase
+{
+    /**
+     * 测试xml解析成数组,仅限于简单的一维xml
+     */
+    public function test1()
+    {
+        $xml = '<xml>
+   <appid>wx2421b1c4370ec43b</appid>
+   <attach>支付测试</attach>
+   <body>APP支付测试</body>
+   <mch_id>10000100</mch_id>
+   <nonce_str>1add1a30ac87aa2db72f57a2375d8fec</nonce_str>
+   <notify_url>http://wxpay.wxutil.com/pub_v2/pay/notify.v2.php</notify_url>
+   <out_trade_no>1415659990</out_trade_no>
+   <spbill_create_ip>14.23.150.211</spbill_create_ip>
+   <total_fee>1</total_fee>
+   <trade_type>APP</trade_type>
+   <sign>0CB01533B8C1EF103065174F50BCA001</sign>
+</xml>';
+        $array = (new XmlToArray())
+            ->setXml($xml)
+            ->__invoke();
+        $this->assertTrue(is_array($array));
+        $this->assertEquals($array, [
+            'appid' => 'wx2421b1c4370ec43b',
+            'attach' => '支付测试',
+            'body' => 'APP支付测试',
+            'mch_id' => '10000100',
+            'nonce_str' => '1add1a30ac87aa2db72f57a2375d8fec',
+            'notify_url' => 'http://wxpay.wxutil.com/pub_v2/pay/notify.v2.php',
+            'out_trade_no' => '1415659990',
+            'spbill_create_ip' => '14.23.150.211',
+            'total_fee' => 1,
+            'trade_type' => 'APP',
+            'sign' => '0CB01533B8C1EF103065174F50BCA001'
+        ]);
+    }
+}
