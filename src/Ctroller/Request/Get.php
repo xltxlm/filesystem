88ef2a7384358ee51f4ef69a892cc02d86b42bce
夏琳泰 <xltxlm@qq.com>
2016-12-24 23:00:42 +0800

    独立CopyObjectAttributeName 类定义都可以取变量名

--- src/Ctroller/Request/Get.php
+++ src/Ctroller/Request/Get.php
@@ -8,6 +8,7 @@
 
 namespace xltxlm\helper\Ctroller\Request;
 
+use xltxlm\helper\Hclass\CopyObjectAttributeName;
 use xltxlm\helper\Hclass\LoadFromArray;
 use xltxlm\helper\Hclass\ObjectToJson;
 
@@ -21,6 +22,7 @@ trait Get
         __construct as load;
     }
     use ObjectToJson;
+    use CopyObjectAttributeName;
 
     /**
      * Get constructor.
@@ -28,20 +30,5 @@ trait Get
     public function __construct()
     {
         $this->load($_GET);
-    }
-
-    /**
-     * 获取变量的名称,用法:要求getxxx() 改为 &getxxxx(),[并且]最后一行是 return $this->xxx; 不能是 return $this->xxx='xx';
-     * @param $var
-     * @return string
-     */
-    final public function varName(&$var)
-    {
-        $tmp = $var;
-        $var = 'tmp_exists_'.uniqid();
-        $name = array_search($var, get_object_vars($this), true);
-        $var = $tmp;
-
-        return $name;
     }
 }
---
+++ src/Hclass/CopyObjectAttributeName.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialintai
+ * Date: 2016/12/24
+ * Time: 22:58
+ */
+
+namespace xltxlm\helper\Hclass;
+
+/**
+ * 如果一个函数返回一个属性,函数前面加上&,即可 用varname(函数名) 取到属性的名称
+ * Class CopyObjectAttributeName
+ * @package xltxlm\helper\Hclass
+ */
+trait CopyObjectAttributeName
+{
+    /**
+     * 获取变量的名称,用法:要求getxxx() 改为 &getxxxx(),[并且]最后一行是 return $this->xxx; 不能是 return $this->xxx='xx';
+     * @param $var
+     * @return string
+     */
+    final public function varName(&$var)
+    {
+        $tmp = $var;
+        $var = 'tmp_exists_'.uniqid();
+        $name = array_search($var, get_object_vars($this), true);
+        $var = $tmp;
+
+        return $name;
+    }
+}
