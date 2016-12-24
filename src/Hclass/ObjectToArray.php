8b535a8548b9d93aa000155b3bd5372d57b7ac8a
夏琳泰 <xltxlm@qq.com>
2016-12-24 18:39:25 +0800

    增加对象转换成数组功能

---
+++ src/Hclass/ObjectToArray.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialintai
+ * Date: 2016/12/24
+ * Time: 18:36
+ */
+
+namespace xltxlm\helper\Hclass;
+
+/**
+ * 把对象转换成数组
+ * Class ObjectToArray
+ * @package xltxlm\helper\Hclass
+ */
+trait ObjectToArray
+{
+    /**
+     * 把对象转换成数组
+     * @return array
+     */
+    final public function __toString()
+    {
+        return get_object_vars($this);
+    }
+}
