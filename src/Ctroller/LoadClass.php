a75a4ecec8136e7843446f71cb34560ecca50b86
夏琳泰 <xltxlm@qq.com>
2017-01-04 17:43:30 +0800

    加载请求类 异常日志

--- src/Ctroller/LoadClass.php
+++ src/Ctroller/LoadClass.php
@@ -100,10 +100,41 @@ final class LoadClass
         spl_autoload_register(function ($class) {
             $request = substr($class, -7);
             if ($request == 'Request') {
-                $filepath = LoadClass::$rootDir.strtr($class, [LoadClass::$rootNamespce => '', '\\' => '/', 'Request' => '.request.php']);
+                $filepath = LoadClass::$rootDir.strtr($class, [LoadClass::$rootNamespce => '', '\\' => '/', 'Request' => '.Request.php']);
                 eval('include_once  $filepath;');
             }
         });
+
+        //异常抛出,追加网址来源
+        set_exception_handler(function ($exception) {
+            /** @var \Exception $exception */
+            $message = [
+                "ERROR" => $exception->getMessage(),
+                "FILE" => $exception->getFile(),
+                "LINE" => $exception->getLine(),
+                'HTTP_REFERER' => $_SERVER['HTTP_REFERER'],
+                'URL' => $_SERVER['REQUEST_URI'],
+                'POST' => $_POST,
+                'IP' => $_SERVER['REMOTE_ADDR'],
+            ];
+            $exceptionS['GET'] = $_GET;
+            $exceptionS['POST'] = $_POST;
+            $exceptionS['URL'] = $_SERVER['REQUEST_URI'];
+            $exceptionS['HTTP_REFERER'] = $_SERVER['HTTP_REFERER'];
+            $exceptionS[] = $message['ERROR']."\t".$message['FILE'].':'.$message['LINE'];
+            $className = "";
+            foreach ($exception->getTrace() as $item) {
+                if (!$className) {
+                    $className = $item['class'];
+                }
+                $exceptionS[] = $item['class'].'::'.
+                    $item['function']."\t".$item['file'].':'.$item['line'];
+            }
+            Util::d($exceptionS);
+            $json_encode = json_encode($message, JSON_UNESCAPED_UNICODE);
+            throw new \Exception($json_encode);
+        });
+
         if (!$this->className) {
             $this->className = '\\'.self::$rootNamespce.'\\'.$this->urlPath;
         }
