4310cf5b850fe2d0f2c007367b61271e59d34e1e
夏琳泰 <xltxlm@qq.com>
2017-02-20 15:51:01 +0800

    优化项目

--- src/Hclass/ConvertObject.php
+++ src/Hclass/ConvertObject.php
@@ -59,12 +59,22 @@ class ConvertObject
     public function toArray()
     {
         if (empty($this->toArray)) {
-            $ReflectionClass = (new \ReflectionClass($this->getObject()));
-            $Properties = $ReflectionClass->getProperties();
+            $Properties = (new \ReflectionClass($this->getObject()))->getProperties();
             /** @var \ReflectionProperty $property */
             foreach ($Properties as $property) {
                 $property->setAccessible(true);
-                $this->toArray[$property->getName()] = $property->getValue($this->getObject());
+                $value = $property->getValue($this->getObject());
+                //支持再下一层的对象
+                if (is_object($value)) {
+                    /** @var \ReflectionProperty $property2 */
+                    $Properties2 = (new \ReflectionClass($value))->getProperties();
+                    foreach ($Properties2 as $property2) {
+                        $property2->setAccessible(true);
+                        $this->toArray[$property->getName()][$property2->getName()] = $property2->getValue($value);
+                    }
+                } else {
+                    $this->toArray[$property->getName()] = $value;
+                }
             }
         }
 
