6616095c7eb618860281862d4a6eba91b3b5011b
夏琳泰 <xltxlm@qq.com>
2017-04-23 16:38:20 +0800

    * 2017-04-19 加强文件,文件夹操作方式

--- src/Hdir/file_write_contents.php
+++ src/Hdir/file_write_contents.php
@@ -18,13 +18,16 @@ trait file_write_contents
     /**
      * @param string $classRealFile
      * @param string $ob_get_clean
+     * @return bool
      */
-    protected function file_write_contents(string $classRealFile, string $ob_get_clean,bool $orverWrite = true)
+    protected function file_write_contents(string $classRealFile, string $ob_get_clean,bool $orverWrite = true):bool
     {
         //1:先保证控制层的基准类一定存在
         $file_get_contents = file_get_contents($classRealFile);
         if (!$file_get_contents || !is_file($classRealFile) || ($file_get_contents !== $ob_get_clean && $orverWrite)) {
             file_put_contents($classRealFile, $ob_get_clean);
+            return true;
         }
+        return false;
     }
 }
